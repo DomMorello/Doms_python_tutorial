6b6eee3ca019b0f37b7ac3b8480b7be4a86a1ba9
donglee <zuzudnf@gmail.com>
2020-07-18 22:18:07 +0900

    control

--- JumpToPython/test.py
+++ JumpToPython/test.py
@@ -1,3 +1,9 @@
-a = "hello w w   w"
-a.split()
-a , b = b, a
+marks = [90,38,58,79,98]
+
+number = 0
+for mark in marks:
+    number += 1
+    if mark >= 60:
+        print("%d pass" % number)
+    else:
+        print("%d fail" % number)
