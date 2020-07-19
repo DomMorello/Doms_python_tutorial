55efa28974e3b95f697ea1219211d2a90544c257
donglee <zuzudnf@gmail.com>
2020-07-19 16:42:12 +0900

    python practice

--- JumpToPython/test.py
+++ JumpToPython/test.py
@@ -1,10 +1,2 @@
 
-def introduce(name, age, man=True):
-    print("name is %s" % name)
-    print("age is %d" % age)
-    if man:
-        print("male")
-    else:
-        print("female")
 
-introduce("donglee", 28, False)
